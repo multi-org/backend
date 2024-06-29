0cc7ae218bcba96e269b24c063c0e7ada0983693
samuelfsd <samuel.estudo778@gmail.com>
2024-06-29 10:47:17 -0300

    chore: configuração inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { app } from './app'
+
+import { env } from '@/env'
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Http server running!!!')
+  })
