3b3eb0c0691605e683fc5a1ae6c55f73bcfcdfba
Gabriel Menezes <gabenesus@gmail.com>
2024-09-29 18:04:56 -0300

    feat: Adicionar inicialização do servidor e conexão com o banco de dados.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
-const a = 1;
-const b = 2;
+import express from "express";
+import conectarNaDB from "./config/dbConnect";
 
-console.log(a + b);
+async function startApp() {
+  try {
+    const conexao = await conectarNaDB(); // Conexão com o banco de dados
+    conexao.on("error", (erro) => {
+      console.error("Erro ao conectar no banco de dados: " + erro);
+    });
+
+    conexao.once("open", () => {
+      console.log("Conexão com o banco de dados realizada com sucesso");
+    });
+  } catch (error) {
+    console.error("Erro durante a inicialização do banco de dados:", error);
+  }
+}
+
+startApp();
+
+const app = express();
+
+export default app;
