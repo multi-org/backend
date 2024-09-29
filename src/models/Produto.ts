f88bd476018170a3db3c6c548e93eb50c13a2304
Gabriel Menezes <gabenesus@gmail.com>
2024-09-29 19:18:58 -0300

    feat: Adicionar modelo de Produto.

---
+++ src/models/Produto.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document } from "mongoose";
+
+interface ProdutoInterface extends Document {
+  nome: string;
+  descricao?: string;
+  categoria: "Espaços" | "Serviços" | "Equipamento";
+  disponibilidade: Array<{ data: Date; horario: string }>;
+  preco: number;
+}
+
+const produtoSchema = new Schema<ProdutoInterface>(
+  {
+    nome: { type: String, required: true, maxlength: 255 },
+    descricao: { type: String },
+    categoria: {
+      type: String,
+      enum: ["Espaços", "Serviços", "Equipamento"],
+      required: true,
+    },
+    disponibilidade: [
+      {
+        data: { type: Date, required: true },
+        horario: { type: String, required: true },
+      },
+    ],
+    preco: { type: Number, required: true, min: 0 },
+  },
+  { timestamps: true },
+);
+
+const Produto = model<ProdutoInterface>("Produto", produtoSchema);
+
+export default Produto;
