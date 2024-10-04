50987c00dbbb0065194d8cf859ad59f7871bcc62
Iago Moreira <iagomoreira2353@gmail.com>
2024-10-03 21:11:39 -0300

    feat: adicionar as rotas para mostrar um produto por nome ou id

--- src/routes/produtosRoutes.ts
+++ src/routes/produtosRoutes.ts
@@ -4,7 +4,8 @@ import ProdutoController from "../controllers/produtoController";
 const routes = express.Router();
 
 routes.get("/produtos", ProdutoController.listarProdutos);
-routes.get("/produtos/nome", ProdutoController.listarProdutoPorNome);
+routes.get("/produtos/nome", ProdutoController.listarProdutoPorNomeOuId);
+routes.get("/produtos/:id", ProdutoController.listarProdutoPorNomeOuId);
 routes.post("/produtos", ProdutoController.cadastrarProduto);
 routes.put("/produtos/:id", ProdutoController.atualizarProduto);
 routes.delete("/produtos/:id", ProdutoController.deletarProduto);
