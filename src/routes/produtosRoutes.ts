f454f3b314639b687e5ed02136a6550ff3ddcdc9
Gabriel Menezes <gabenesus@gmail.com>
2024-10-10 11:06:37 -0300

    refactor: Remover rota de remoção de produto por nome e resolver problema de id sendo uma string

--- src/routes/produtosRoutes.ts
+++ src/routes/produtosRoutes.ts
@@ -8,7 +8,6 @@ routes.get("/produtos/nomeOuId", ProdutoController.listarProdutoPorNomeOuId);
 routes.post("/produtos", ProdutoController.cadastrarProduto);
 routes.put("/produtos/:id", ProdutoController.atualizarProduto);
 routes.delete("/produtos/:id", ProdutoController.deletarProduto);
-routes.delete("/produtos/nome", ProdutoController.deletarProduto);
 
 export default routes;
 
