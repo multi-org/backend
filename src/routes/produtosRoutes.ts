a83d2e1119cc15f40cc73ec57f31e2acdc9c8654
Gabriel Menezes <gabenesus@gmail.com>
2024-10-10 10:15:12 -0300

    refactor: Atualizar rota de busca de produtos por nome ou ID.

--- src/routes/produtosRoutes.ts
+++ src/routes/produtosRoutes.ts
@@ -4,11 +4,12 @@ import ProdutoController from "../controllers/produtoController";
 const routes = express.Router();
 
 routes.get("/produtos", ProdutoController.listarProdutos);
-routes.get("/produtos/nome", ProdutoController.listarProdutoPorNomeOuId);
-routes.get("/produtos/:id", ProdutoController.listarProdutoPorNomeOuId);
+routes.get("/produtos/nomeOuId", ProdutoController.listarProdutoPorNomeOuId);
 routes.post("/produtos", ProdutoController.cadastrarProduto);
 routes.put("/produtos/:id", ProdutoController.atualizarProduto);
 routes.delete("/produtos/:id", ProdutoController.deletarProduto);
 routes.delete("/produtos/nome", ProdutoController.deletarProduto);
 
 export default routes;
+
+// Busca por nome e ID utilizam de query, ou seja a rota de ambas devem ser busca por query.
