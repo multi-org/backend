4a9eff3db817ee71b1bd85b0842cabf8934b4314
Giordanni <giordanniformiga103@gmail.com>
2025-06-19 21:56:34 -0300

    Padronizando forma de criar usuário com modelo do figma e criando as rotas de login, logout e getMe

--- src/controllers/user_controllers.ts
+++ src/controllers/user_controllers.ts
@@ -1,12 +1,62 @@
 import { AuthRequest } from '@app/middlewares/global_middleware'
 import { Request, Response } from 'express';
-import {createUserZode} from '@app/models/models_user'
+import {createUserZode, emailZode} from '@app/models/models_user'
 import UserService from '@app/services/user_services';
 
 class userController {
 
+    async sendCodeToEmail(req: AuthRequest, res: Response) {
+        const { email } = req.body;
+        
+        const result = emailZode.safeParse(email);
+        if (!result.success) {
+            return res.status(400).json({
+                message: "Invalid email format",
+            });
+        }
+        try {
+            const response = await UserService.sendCodeToEmail(email);
+
+            res.cookie('email', response.email, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 1000,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'lax',
+                path: '/'
+            });
+
+            return res.status(200).json(response);
+        } catch (error: any) {
+            const statusCode = error.status || 500;
+            return res.status(statusCode).json({
+                message: error.message || "Internal Server Error",
+            });
+        }
+    }
+
+    async validEmail(req: AuthRequest, res: Response) {
+        const { email } = req.cookies;
+        if (!email) {
+            return res.status(500).json({ message: "Email not found in cookies" });
+        }
+        
+        try {
+            const result = await UserService.validEmail(email, req.body);
+            return res.status(200).json(result.message);
+        } catch (error: any) {
+            const statusCode = error.status || 500;
+            return res.status(statusCode).json({
+                message: error.message || "Internal Server Error",
+            });
+        }
+    }
+
     async createUser(req: Request, res: Response) {
         const result = createUserZode.safeParse(req.body);
+        const email = req.cookies.email;
+        if (!email) {
+            return res.status(500).json({ message: "Email not found in cookies" });
+        }
 
         if (!result.success) {
             const errors = result.error.flatten().fieldErrors;
@@ -18,12 +68,15 @@ class userController {
         try {
             const user = await UserService.createUser({
                 ...result.data,
-                isEmailVerified: false
-            });
+                isEmailVerified: true,
+                email: email
+            }, req.body.confirmPassword);
 
-            res.cookie('email', user.email, {
+            res.clearCookie("email", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax', path: '/' });
+
+            res.cookie("token", user.token, {
                 httpOnly: true,
-                maxAge: 20 * 60 * 1000,
+                maxAge: 6 * 60 * 60 * 1000, // 6 horas
                 secure: process.env.NODE_ENV === 'production',
                 sameSite: 'lax',
                 path: '/'
@@ -38,27 +91,52 @@ class userController {
         }
     }
 
-    async validationEmailAndCreateUser(req: AuthRequest, res: Response) {
-        const { email } = req.cookies;
-
-        if (!email) {
-            return res.status(500).json({ message: "Email not found in cookies" });
+    async login(req: Request, res: Response) {
+        const { email } = req.body;
+        const result = emailZode.safeParse(email);
+        if (!result.success) {
+            return res.status(400).json({
+                message: "Invalid email format",
+            });
         }
-
         try {
-            const user = await UserService.validEmailAndCreateUser(email, req.body);
-
-            res.clearCookie("email", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax', path: '/' });
-            
+            const user = await UserService.login(email, req.body.password);
             res.cookie("token", user.token, {
                 httpOnly: true,
                 maxAge: 6 * 60 * 60 * 1000, // 6 horas
                 secure: process.env.NODE_ENV === 'production',
                 sameSite: 'lax',
                 path: '/'
-            });
+            })
 
-            return res.status(200).json({ message: user.message, userName: user.userName });
+            res.status(200).json({
+                message: "Login successful",
+                userName: user.userName,
+            });
+        }catch (error: any) {
+            const statusCode = error.status || 500;
+            return res.status(statusCode).json({
+                message: error.message || "Internal Server Error",
+            });
+        }
+    }
+
+    async logout(req: Request, res: Response) {
+        res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax', path: '/' });
+        res.clearCookie("email", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'lax', path: '/' });
+        return res.status(200).json({ message: "Logout successful" });
+    }
+
+    async getMe(req: AuthRequest, res: Response) {
+        
+        const userId = req.userId; 
+
+        try {
+            if (!userId) {
+                return res.status(400).json({ message: "User ID not found in cookies" });
+            }
+            const user = await UserService.getMe(userId);
+            return res.status(200).json(user);
         } catch (error: any) {
             const statusCode = error.status || 500;
             return res.status(statusCode).json({
@@ -66,6 +144,7 @@ class userController {
             });
         }
     }
+
 
 }
 
--- src/repositories/user_repository.ts
+++ src/repositories/user_repository.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { createUserDTOS } from '@app/models/models_user';
+import { UUID } from "crypto";
 
 
 const prisma = new PrismaClient();
 
 export class UserRepository {
 
-    async createUser(userData: createUserDTOS & { isEmailVerified: boolean }) {
+    async createUser(userData: createUserDTOS) {
         const user = await prisma.user.create({
             data: userData
         })
@@ -17,6 +18,16 @@ export class UserRepository {
         const user = await prisma.user.findFirst({
             where: {
                 email: email,
+                status: 'ACTIVE'
+            }
+        })
+        return user;
+    }
+
+    async findUserById(userId: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                userId: userId,
                 status: 'ACTIVE'
             }
         })
