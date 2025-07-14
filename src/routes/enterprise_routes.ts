b31e6bfe56f9d33aca4b14c5110ee587303f1c89
Giordanni <giordanniformiga103@gmail.com>
2025-07-14 19:00:40 -0300

    consertando rota

--- src/routes/enterprise_routes.ts
+++ src/routes/enterprise_routes.ts
@@ -12,7 +12,7 @@ enterpriseRoutes.get('/invite/accept', jwtRequired, enterpriseController.acceptI
 enterpriseRoutes.post('/invite/:companyId', jwtRequired, checkPermission('invite:legal_representative'), enterpriseController.inviteLegalRepresentative);
 
 enterpriseRoutes.post('/request/registration', jwtRequired, checkPermission('request_registration:company'), enterpriseController.requestCompanyRegistrationData);
-enterpriseRoutes.get('/all/requests', jwtRequired, checkCompanyPermission('get_all_requests:company'), enterpriseController.getAllCompanyRequest);
+enterpriseRoutes.get('/all/requests', jwtRequired, checkPermission('get_all_requests:company'), enterpriseController.getAllCompanyRequest);
 enterpriseRoutes.post('/confirm/:cnpj', jwtRequired, checkPermission('create:company'), enterpriseController.confirmCompanyCreationData);
 
 
