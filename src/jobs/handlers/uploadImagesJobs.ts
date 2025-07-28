84506b8d794e97fa96b368a17c9751e671a2ef4a
Giordanni <giordanniformiga103@gmail.com>
2025-07-28 19:39:44 -0300

    colocando o delete das requisições de associados

--- src/jobs/handlers/uploadImagesJobs.ts
+++ src/jobs/handlers/uploadImagesJobs.ts
@@ -33,16 +33,28 @@ export const uploadUserProfileImageJob = {
 };
 
 export const uploadDocumentPdfJob = {
-  key: 'uploadDocumentPdf',
-  async handle({ data }: {
-    data: { localFilePath: string; userId: string; userCpf: string; companyId: string }
+  key: "uploadDocumentPdf",
+  async handle({
+    data,
+  }: {
+    data: {
+      localFilePath: string;
+      userId: string;
+      userCpf: string;
+      companyId: string;
+    };
   }) {
     const { localFilePath, userId, userCpf, companyId } = data;
 
     try {
       logger.info(`Job: Starting uploadDocumentPdf for user`);
-      
-      const result = await uploadService.uploadDocumentPdf(localFilePath, userId, userCpf, companyId);
+
+      const result = await uploadService.uploadDocumentPdfAssociation(
+        localFilePath,
+        userId,
+        userCpf,
+        companyId
+      );
 
       logger.info(`Job: uploadDocumentPdf completed successfully for user`);
 
@@ -50,44 +62,60 @@ export const uploadDocumentPdfJob = {
         success: true,
         result: result.secure_url,
         cloudinaryId: result.public_id,
-        userId: userId
+        userId: userId,
       };
-
     } catch (error) {
-      logger.error('Job: Error in uploadDocumentPdf', { error });
-      throw new CustomError('Erro ao fazer upload do documento PDF', 500);
+      logger.error("Job: Error in uploadDocumentPdf", { error });
+      throw new CustomError("Erro ao fazer upload do documento PDF", 500);
     }
-  }
+  },
 };
 
 export const uploadProductImagesJob = {
-  key: 'uploadProductImages',
-  async handle({ data }: { data: { productId: string; imagePaths: string[] } }) {
+  key: "uploadProductImages",
+  async handle({
+    data,
+  }: {
+    data: { productId: string; imagePaths: string[] };
+  }) {
     const { productId, imagePaths } = data;
 
     try {
-      logger.info(`Job: Starting uploadProductImages for product`, { productId });
+      logger.info(`Job: Starting uploadProductImages for product`, {
+        productId,
+      });
 
-      const uploadedImageUrls = await uploadService.uploadProductImage(imagePaths, productId);
+      const uploadedImageUrls = await uploadService.uploadProductImage(
+        imagePaths,
+        productId
+      );
 
       if (uploadedImageUrls.length > 0) {
-        await productRepository.updateProduct(productId, { imagesUrls: uploadedImageUrls });
-        logger.info(`Product ${productId} updated with ${uploadedImageUrls.length} image URLs.`);
+        await productRepository.updateProduct(productId, {
+          imagesUrls: uploadedImageUrls,
+        });
+        logger.info(
+          `Product ${productId} updated with ${uploadedImageUrls.length} image URLs.`
+        );
       } else {
-        logger.warn(`No images were successfully uploaded for product ${productId}.`);
+        logger.warn(
+          `No images were successfully uploaded for product ${productId}.`
+        );
       }
 
-      logger.info(`Job: uploadProductImages completed successfully for product`, { productId });
+      logger.info(
+        `Job: uploadProductImages completed successfully for product`,
+        { productId }
+      );
 
       return {
         success: true,
         productId: productId,
-        uploadedImageUrls: uploadProductImagesJob
+        uploadedImageUrls: uploadProductImagesJob,
       };
-      
     } catch (error) {
-      logger.error('Job: Error in uploadProductImages', { error });
-      throw new CustomError('Erro ao fazer upload das imagens do produto', 500);
+      logger.error("Job: Error in uploadProductImages", { error });
+      throw new CustomError("Erro ao fazer upload das imagens do produto", 500);
     }
   },
-}
+};
--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -14,8 +14,11 @@ userRoutes.post('/logout', jwtRequired, userController.logout);
 userRoutes.get('/me', jwtRequired, userController.getMe);
 userRoutes.post('/address', jwtRequired, userController.createAddress);
 
+// para associados
 userRoutes.post('/request/association/:companyId', jwtRequired, uploadDocuments.single('document'), userController.requestAssociation);
 userRoutes.get("/all/associations/request", jwtRequired, checkPermission("get_all_requests:company_associate"), userController.getAllAssociations);
 userRoutes.post("/association/confirmation/:userId/:companyId", jwtRequired, checkCompanyPermission('accept:company_associate'), userController.associationToCompanyConfirmation);
+userRoutes.delete("/association/reject/:userId/:companyId", jwtRequired, checkCompanyPermission('reject:company_associate'), userController.associationToCompanyReject);
+userRoutes.delete("/all/association/reject", jwtRequired, checkCompanyPermission('reject:company_associate'), userController.deleteAllAssociationRequests);
 
 export default userRoutes;
