3a237ac88aad3fa84b07dad6912ce90ff24ed4a6
Giordanni <giordanniformiga103@gmail.com>
2025-09-15 11:27:30 -0300

    apagando o upload

--- src/routes/main_routes.ts
+++ src/routes/main_routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import userRoutes from "./user_routes";
 import enterpriseRoutes from "./enterprise_routes";
 import productRoutes from "./products_routes"
-import uploadRoutes from './upload_routes';
 import availabilityRoutes from './availability_routes';
 
 const router = Router();
@@ -12,6 +11,5 @@ router.use("/companies", enterpriseRoutes);
 router.use("/availability", availabilityRoutes);
 
 router.use("/products", productRoutes);
-router.use("/upload", uploadRoutes);
 
 export default router;
