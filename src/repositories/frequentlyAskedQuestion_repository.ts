cd6a3c2b6241b24b01019c20c3231141e38a41bb
Giordanni <giordanniformiga103@gmail.com>
2025-09-16 09:33:00 -0300

    finalizando o CRUS de perguntas frequentes

--- src/repositories/frequentlyAskedQuestion_repository.ts
+++ src/repositories/frequentlyAskedQuestion_repository.ts
@@ -13,6 +13,42 @@ class FrequentlyAskedQuestionRepository {
             }
         });
     }
+
+    async GetAllQuestions() {
+        return await this.prisma.frequentlyAskedQuestion.findMany();
+    }
+
+    async getQuestionById(id: string) {
+        return await this.prisma.frequentlyAskedQuestion.findUnique({
+            where: { id }
+        });
+    }
+
+    async updateQuestion(id: string, question: string) {
+        return await this.prisma.frequentlyAskedQuestion.update({
+            where: { id },
+            data: { question }
+        });
+    }
+
+    async answerQuestion(id: string, answer: string) {
+        return await this.prisma.frequentlyAskedQuestion.update({
+            where: { id },
+            data: { answer}
+        })
+    }
+
+    async deleteQuestion(id: string) {
+        return await this.prisma.frequentlyAskedQuestion.delete({
+            where: { id }
+        });
+    }
+
+    async getAllUnansweredQuestions() {
+        return await this.prisma.frequentlyAskedQuestion.findMany({
+            where: { answer: null}
+        })
+    }
 }
 
 export default new FrequentlyAskedQuestionRepository();
--- src/services/frequentlyAskedQuestion_services.ts
+++ src/services/frequentlyAskedQuestion_services.ts
@@ -1,5 +1,4 @@
 import questionFrequentily from "@app/repositories/frequentlyAskedQuestion_repository";
-import userService from "./user_services";
 import userRepository from "@app/repositories/user_repository";
 import {logger, CustomError} from "@app/utils/logger";
 
@@ -26,6 +25,120 @@ class FrequentlyAskedQuestionServices {
 
         logger.info("Frequently asked question created successfully");
         return {success: true};
+    }
+
+    async getAllQuestions() {
+        logger.info("Fetching all frequently asked questions");
+
+        const questions = await questionFrequentily.GetAllQuestions();
+
+        if (!questions) {
+            logger.warn("No questions found");
+            throw new CustomError("No questions found", 404);
+        }
+
+        logger.info("Frequently asked questions fetched successfully");
+        return questions;
+    }
+
+    async getQuestionById(id: string) {
+        logger.info(`Fetching question with ID: ${id}`);
+
+        const question = await questionFrequentily.getQuestionById(id);
+
+        if (!question) {
+            logger.warn(`Question with ID: ${id} not found`);
+            throw new CustomError("Question not found", 404);
+        }
+
+        logger.info(`Question with ID: ${id} fetched successfully`);
+        return question;
+    }
+
+    async updateQuestion(id: string, question: string) {
+        logger.info(`Updating question with ID: ${id}`);
+
+        const existingQuestion = await questionFrequentily.getQuestionById(id);
+        if (!existingQuestion) {
+            logger.warn(`Question with ID: ${id} not found`);
+            throw new CustomError("Question not found", 404);
+        }
+
+        if (question.trim().length === 0 || !question) {
+            logger.warn("Question cannot be empty or undefined");
+            throw new CustomError("Question cannot be empty or undefined", 400);
+        }
+
+        const updatedQuestion = await questionFrequentily.updateQuestion(id, question);
+        if (!updatedQuestion) {
+            logger.error(`Failed to update question with ID: ${id}`);
+            throw new CustomError("Failed to update the question", 500);
+        }
+
+        if (updatedQuestion.question === existingQuestion.question) {
+            logger.warn("No changes detected in the question");
+            throw new CustomError("No changes detected in the question", 400);
+        }
+
+        logger.info(`Question with ID: ${id} updated successfully`);
+        return updatedQuestion;
+    }
+
+    async answerQuestion(id: string, answer: string) {
+        logger.info(`Answering question with ID: ${id}`);
+
+        const existingQuestion = await questionFrequentily.getQuestionById(id);
+        if (!existingQuestion) {
+            logger.warn(`Question with ID: ${id} not found`);
+            throw new CustomError("Question not found", 404);
+        }
+
+        if (answer.trim().length === 0 || !answer) {
+            logger.warn("Answer cannot be empty or undefined");
+            throw new CustomError("Answer cannot be empty or undefined", 400);
+        }
+
+        const answeredQuestion = await questionFrequentily.answerQuestion(id, answer);
+        if (!answeredQuestion) {
+            logger.error(`Failed to answer question with ID: ${id}`);
+            throw new CustomError("Failed to answer the question", 500);
+        }
+
+        logger.info(`Question with ID: ${id} answered successfully`);
+        return answeredQuestion;
+    }
+
+    async deleteQuestion(id: string) {
+        logger.info(`Deleting question with ID: ${id}`);
+
+        const existingQuestion = await questionFrequentily.getQuestionById(id);
+        if (!existingQuestion) {
+            logger.warn(`Question with ID: ${id} not found`);
+            throw new CustomError("Question not found", 404);
+        }
+
+        const deletedQuestion = await questionFrequentily.deleteQuestion(id);
+        if (!deletedQuestion) {
+            logger.error(`Failed to delete question with ID: ${id}`);
+            throw new CustomError("Failed to delete the question", 500);
+        }
+
+        logger.info(`Question with ID: ${id} deleted successfully`);
+        return {success: true};
+    }
+
+    async getAllUnansweredQuestions() {
+        logger.info("Fetching all unanswered frequently asked questions");
+
+        const questions = await questionFrequentily.getAllUnansweredQuestions();
+
+        if (!questions) {
+            logger.warn("No unanswered questions found");
+            throw new CustomError("No unanswered questions found", 404);
+        }
+
+        logger.info("Unanswered frequently asked questions fetched successfully");
+        return questions;
     }
 }
 
