6a0bce63a80aaf0b0d1c8945fc8289745a649c79
Giordanni <giordanniformiga103@gmail.com>
2025-09-24 08:42:57 -0300

    colocando secure como false para o deploy

--- src/utils/emailConfig.ts
+++ src/utils/emailConfig.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
     host: String(process.env.EMAIL_HOST),
     port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false,
     auth: {
         user: String(process.env.EMAIL_USER),
         pass: String(process.env.EMAIL_PASSWORD)
